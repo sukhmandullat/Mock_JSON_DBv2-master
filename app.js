6be8bfd9bab897eb38f15b5371aa0ee26faff072
sukhmandullat <sukhmandullat52@gmail.com>
2025-03-20 17:15:16 -0500

    Update

--- app.js
+++ app.js
@@ -9,18 +9,6 @@ const PORT = process.env.PORT;
 app.set('view engine','ejs');
 app.set('views', './views');
 
-//Middleware
-app.use((req, res, next)=>{
-    const time = new Date();
-
-    console.log(time, req.url, req.method);
-
-    next();
-});
-
-app.get('/home', (req, res) =>{
-   console.log('This is always triggered secondly.')
-});
 
 
 app.listen(PORT, ()=>{
